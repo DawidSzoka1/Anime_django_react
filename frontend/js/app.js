52c66c08157a0bdca80fef6ac6088dc69fb18a95
DawidSzoka1 <oficer130@gmail.com>
2024-04-13 14:32:11 +0200

    Initial commit Django and React

---
+++ frontend/js/app.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+
+
+const App = () => {
+
+
+};
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
