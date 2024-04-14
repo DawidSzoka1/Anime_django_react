bfaf60ef0eaf59ecf97fa9d6bee75dfa2a4bcc27
DawidSzoka1 <oficer130@gmail.com>
2024-04-14 20:42:03 +0200

    layout for sing up page

---
+++ frontendReact/src/pages/signup.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class SignUp extends React.Component{
+
+    render() {
+        return (
+            <div>
+                future signup page
+            </div>
+        )
+    }
+
+}
+export default SignUp;
