42303f8ebf7b69dd325ef36441f639269853cb58
DawidSzoka1 <oficer130@gmail.com>
2024-04-14 20:42:39 +0200

    navbar component with bootstrap

--- frontendReact/src/components/Navbar.js
+++ frontendReact/src/components/Navbar.js
@@ -1,15 +1,24 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-class Navbar extends React.Component{
+
+class NavbarComp extends React.Component{
     render() {
         return (
-            <>
-
-            </>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="/">MainPage</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link href="/sign-up">Sign up</Nav.Link>
+                        </Nav>
+                </Container>
+            </Navbar>
         )
     }
 
 
 }
 
-export default Navbar
+export default NavbarComp
