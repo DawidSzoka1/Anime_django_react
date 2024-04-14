53e8f6cc0b9bb9c5cc927acef50b6f35f8f26e23
DawidSzoka1 <oficer130@gmail.com>
2024-04-14 17:22:22 +0200

    testing model and serializer

--- anime_backend/djangoProject/mainPage/views.py
+++ anime_backend/djangoProject/mainPage/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import React
+from .serializer import AnimeSerializer
 
 
 # Create your views here.
 
 class MainPageView(APIView):
     def get(self, request):
-        return Response("Hello World")
+        output = [{'title': output.title}
+                  for output in React.objects.all()]
+        return Response(output)
+
+    def post(self, request):
+        serializer = AnimeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
