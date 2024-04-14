db6dbdf620566fb6c7a738761e181b0dc4f392b3
DawidSzoka1 <oficer130@gmail.com>
2024-04-14 20:29:40 +0200

    Navbar component

---
+++ frontendReact/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Navbar extends React.Component{
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+
+
+}
+
+export default Navbar
