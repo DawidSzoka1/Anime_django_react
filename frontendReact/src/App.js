1c7c5fa7e57b8de0e4abc7234c25340879954cf0
DawidSzoka1 <oficer130@gmail.com>
2024-04-14 17:37:59 +0200

    class App

--- frontendReact/src/App.js
+++ frontendReact/src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
+import axios from "axios";
+import React from "react";
 
-const App = () =>  {
-  return (
-    <div className="App">
-     Test
-    </div>
-  );
+class App extends React.Component{
+    state = {details : [], }
+    componentDidMount() {
+        let data;
+        axios.get('http://localhost:8000')
+            .then(res =>{
+                data = res.data;
+                this.setState({
+                    details: data
+                })
+            })
+            .catch(err => {})
+
+    }
+    render() {
+        return (
+            <div>
+                <header>data from django</header>
+                <hr></hr>
+                {this.state.details.map((output, id) => (
+                    <div key={id}>
+                        <h2>{output.title}</h2>
+
+                    </div>
+                ))}
+            </div>
+        )
+    }
+
+
 }
+
 
 export default App;
