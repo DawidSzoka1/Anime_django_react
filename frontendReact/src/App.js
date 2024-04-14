16406363be11e4428b8ff01f29c8f7b6be22c17c
DawidSzoka1 <oficer130@gmail.com>
2024-04-14 20:43:08 +0200

    cleaning code

--- frontendReact/src/App.js
+++ frontendReact/src/App.js
@@ -1,21 +1,22 @@
-import axios from "axios";
 import React from "react";
 import Home from './pages/home'
+import Signup from "./pages/signup";
 import {
     BrowserRouter as Router,
     Routes,
     Route,
 } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import NavbarComp from "./components/Navbar";
 
 class App extends React.Component {
     render() {
         return (
 
             <Router>
-                <Navbar/>
+                <NavbarComp />
                 <Routes>
                     <Route exact path="/" element={<Home/>}/>
+                    <Route path="/sign-up" element={<Signup />}/>
                 </Routes>
             </Router>
 
--- frontendReact/src/index.js
+++ frontendReact/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import './index.css';
 import App from './App'
 
 
