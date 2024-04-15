e65fae1d65a64e4b119ea5de534530ab3999d84e
DawidSzoka1 <oficer130@gmail.com>
2024-04-15 18:58:06 +0200

    Passwordinput component

---
+++ frontendReact/src/components/PasswordInput.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {InputGroup} from "react-bootstrap";
+
+
+class PasswordInput extends React.Component{
+
+    render() {
+        return(
+            <InputGroup>
+
+            </InputGroup>
+        )
+    }
+
+
+}
+
+export default PasswordInput;
