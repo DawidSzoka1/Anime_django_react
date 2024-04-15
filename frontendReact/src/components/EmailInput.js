dc5ddbccc8ad6906566fc1ae4abb924741c1c988
DawidSzoka1 <oficer130@gmail.com>
2024-04-15 18:57:56 +0200

    EmailInput component

---
+++ frontendReact/src/components/EmailInput.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {InputGroup} from "react-bootstrap";
+
+
+class EmailInput extends React.Component{
+
+    render() {
+        return(
+            <InputGroup>
+
+            </InputGroup>
+        )
+    }
+
+
+}
+
+export default EmailInput;
