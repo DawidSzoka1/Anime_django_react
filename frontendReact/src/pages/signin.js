a5e0c8c72f41b4794968038fdbcff5fc7b8782c7
DawidSzoka1 <oficer130@gmail.com>
2024-04-15 18:05:57 +0200

    Inputs email and password

--- frontendReact/src/pages/signin.js
+++ frontendReact/src/pages/signin.js
@@ -1,10 +1,20 @@
 import React from "react";
-
+import {InputGroup, Form} from "react-bootstrap";
 class SignIn extends React.Component {
     render() {
         return (
             <div>
-                future log in
+                <InputGroup className="mt-3">
+                    <InputGroup.Text id="Email">
+                        <Form.Control placeholder="Email" />
+                    </InputGroup.Text>
+                </InputGroup>
+                <InputGroup className="mt-3">
+                    <InputGroup.Text id="Password">
+                        <Form.Control placeholder="Password" />
+                    </InputGroup.Text>
+                </InputGroup>
+
             </div>
         )
     }
