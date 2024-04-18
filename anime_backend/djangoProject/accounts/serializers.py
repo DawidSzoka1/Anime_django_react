3c1b17ff1f31dfc5d473b218c652274a52045814
DawidSzoka1 <oficer130@gmail.com>
2024-04-18 13:34:05 +0200

    creat in serializer and generic creat user view

--- anime_backend/djangoProject/accounts/serializers.py
+++ anime_backend/djangoProject/accounts/serializers.py
@@ -1,8 +1,12 @@
-from accounts.models import User
+from .models import User
 from rest_framework import serializers
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['id', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- anime_backend/djangoProject/accounts/views.py
+++ anime_backend/djangoProject/accounts/views.py
@@ -1,22 +1,16 @@
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import User
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import RegisterSerializer
 
 
-class RegisterView(APIView):
-    serializer_class = RegisterSerializer
+class RegisterView(generics.CreateAPIView):
+    serializer_class = User
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
 
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            email = serializer.validated_data.get('email')
-            password = serializer.validated_data.get('password')
-            User.objects.create_user(email=email, password=password)
-
-        Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 
