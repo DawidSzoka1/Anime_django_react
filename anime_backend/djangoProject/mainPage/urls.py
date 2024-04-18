2c1cc27b8b8f157cbcca2beb0d753da9a9bc3caa
DawidSzoka1 <oficer130@gmail.com>
2024-04-18 14:48:30 +0200

    added url to test fetching data from api

--- anime_backend/djangoProject/mainPage/urls.py
+++ anime_backend/djangoProject/mainPage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MainPageView.as_view(), name='index')
+    path('', views.MainPageView.as_view(), name='index'),
+    path('testAnime/', views.AnimeApiTest.as_view(), name='testAnime')
 ]
 
