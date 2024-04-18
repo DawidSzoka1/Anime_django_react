7ae8157cce61c459e3573095da090b24aa545b76
DawidSzoka1 <oficer130@gmail.com>
2024-04-18 14:49:50 +0200

    added testing view for fetching anime data and view to save that data to Anime table still in progres

--- anime_backend/djangoProject/mainPage/views.py
+++ anime_backend/djangoProject/mainPage/views.py
@@ -1,20 +1,46 @@
+from django.contrib.sites import requests
+from django.views import View
 from rest_framework.views import APIView
+import requests
 from rest_framework.response import Response
-from .models import React
-from .serializer import AnimeSerializer
+from django.http import JsonResponse
+from .models import Anime
+import json
 
 
 # Create your views here.
 
-class MainPageView(APIView):
+class MainPageView(View):
     def get(self, request):
-        output = [{'title': output.title, 'test': output.test}
-                  for output in React.objects.all()]
-        return Response(output)
+        api_url = 'https://api.jikan.moe/v4/anime'
+        json_data = 0
+        try:
+            response = requests.get(api_url)
+            if response.status_code == 200:
+                json_data = response.json()
+        except requests.RequestException:
+            pass
+        # for anime in json_data['data']:
+        #     if anime not in Anime.objects.all():
+        #         Anime.objects.create(anime_data=anime)
+        # for anime in json_data:
+        #     json_data = anime
+        names = [anime['title'] for anime in json_data['data']]
 
-    def post(self, request):
-        serializer = AnimeSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data)
+        return JsonResponse({'test': f"{names}"}, safe=False)
 
+
+
+
+class AnimeApiTest(View):
+    def get(self, request):
+        api_url = 'https://api.jikan.moe/v4/anime'
+
+        try:
+            response = requests.get(api_url)
+            if response.status_code == 200:
+                return JsonResponse(response.json(), safe=False)
+            else:
+                return JsonResponse({'error': 'Nie udało się pobrać danych z API'}, status=500)
+        except requests.RequestException as e:
+            return JsonResponse({'error': 'Wystąpił błąd podczas wysyłania zapytania do API'}, status=500)
