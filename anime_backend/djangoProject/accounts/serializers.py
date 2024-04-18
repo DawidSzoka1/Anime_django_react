6caa3c86e7834d3b3532c2968e6ae2352b8bbb57
DawidSzoka1 <oficer130@gmail.com>
2024-04-18 14:48:05 +0200

    added write only to password

--- anime_backend/djangoProject/accounts/serializers.py
+++ anime_backend/djangoProject/accounts/serializers.py
@@ -6,6 +6,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
--- anime_backend/djangoProject/mainPage/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-
-class AnimeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = React
-        fields = ['title', 'test']
