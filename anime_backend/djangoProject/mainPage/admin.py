9fbeea926476e52572a647f25c35ec64db85eb7a
DawidSzoka1 <oficer130@gmail.com>
2024-04-19 15:45:12 +0200

    admin panel to see existing users

--- anime_backend/djangoProject/mainPage/admin.py
+++ anime_backend/djangoProject/mainPage/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from accounts.models import User
+
 
 # Register your models here.
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+    list_filter = ('email',)
+    search_fields = ('email',)
+
+
+admin.site.register(User, UsersAdmin)
