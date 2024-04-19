ef97ee02c0a1c56ff86856792362627d64506ff0
DawidSzoka1 <oficer130@gmail.com>
2024-04-19 15:27:12 +0200

    sending objects to main page to do celery to fetch data once a day

--- anime_backend/djangoProject/mainPage/views.py
+++ anime_backend/djangoProject/mainPage/views.py
@@ -1,5 +1,6 @@
 from django.contrib.sites import requests
 from django.views import View
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 import requests
 from rest_framework.response import Response
@@ -8,44 +9,52 @@ from .models import Anime
 import json
 
 
+def get_anime():
+    api_url = 'https://api.jikan.moe/v4/anime/?per_page=100'
+    json_data = 0
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            json_data = response.json()
+    except requests.RequestException:
+        pass
+    # for anime in json_data['data']:
+    #     if not Anime.objects.filter(name=anime['title']).exists():
+    #         Anime.objects.create(
+    #             name=anime['title'],
+    #             description=anime['synopsis'],
+    #             img=anime['images']['jpg']['image_url'],
+    #             trailer=anime['trailer']['url'],
+    #             status=anime['airing'],
+    #             aired=anime['aired']['string'],
+    #             duration=anime['duration'],
+    #             episodes=anime['episodes'],
+    #             redirect=anime['url'],
+    #             broadcast=anime['broadcast']['string'],
+    #             rank=anime['rank'],
+    #             rating=anime['rating'],
+    #             studio_name=anime['studios'][0]['name'],
+    #             type=anime['type']
+    #
+    #         )
+
+
 # Create your views here.
 
-class MainPageView(View):
+class MainPageView(APIView):
+    permission_classes = [AllowAny]
+
     def get(self, request):
-        api_url = 'https://api.jikan.moe/v4/anime?anime_search_query_status=airing/'
-        json_data = 0
-        try:
-            response = requests.get(api_url)
-            if response.status_code == 200:
-                json_data = response.json()
-        except requests.RequestException:
-            pass
-        # for anime in json_data['data']:
-        #     if not Anime.objects.filter(name=anime['title']).exists():
-        #         Anime.objects.create(
-        #             name=anime['title'],
-        #             description=anime['synopsis'],
-        #             img=anime['images']['jpg']['image_url'],
-        #             trailer=anime['trailer']['url'],
-        #             status=anime['airing'],
-        #             aired=anime['aired']['string'],
-        #             duration=anime['duration'],
-        #             episodes=anime['episodes'],
-        #             redirect=anime['url'],
-        #             broadcast=anime['broadcast']['string'],
-        #             rank=anime['rank'],
-        #             rating=anime['rating'],
-        #             studio_name=anime['studios'][0]['name'],
-        #             type=anime['type']
-        #
-        #         )
+        data = [
+            {'name': anime.name,
+             'description': anime.description,
+             'img': anime.img,
+             'trailer': anime.trailer,
 
-        # names = [anime['title'] for anime in json_data['data']]
-        # names = [anime['aired']['from'] for anime in json_data['data']]
-        # names = [anime['aired']['to'] for anime in json_data['data']]
-        # names = [anime['synopsis'] for anime in json_data['data']]
-
-        return JsonResponse({'test': f"{json_data}"}, safe=False)
+             }
+            for anime in Anime.objects.all()
+        ]
+        return JsonResponse(data, safe=False)
 
 
 class AnimeApiTest(View):
